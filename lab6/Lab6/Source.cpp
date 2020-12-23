7c4ed6e93169ee47f5ded8f844b5be70c7e178b6
Andrew <yamkoviya@gmail.com>
2020-12-23 03:10:13 +0200

    finally

--- lab6/Lab6/Source.cpp
+++ lab6/Lab6/Source.cpp
@@ -11,14 +11,14 @@ int resultOfSum(int, int);
 int func1(int a, int b)
 {
 	int res = 0;
-		for (int i = 0; i < 10; i++)
-		{
-			Sleep(1);
-			if (i > 8)
-				res = resultOfSum(a, b);
-			if (res > 0)
-				return res;
-		}
+	for (int i = 0; i < 10; i++)
+	{
+		Sleep(1);
+		if (i > 8)
+			res = resultOfSum(a, b);
+		if (res > 0)
+			return res;
+	}
 	return res;
 }
 int func2(int a, int b)
